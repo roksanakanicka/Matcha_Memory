66aefbbb60a537295ca66626c5065d5c1067dcfe
Borowaa <mateo.borw.01@gmail.com>
2026-01-22 01:20:01 +0100

    Add-main-menu-scene-and-supporting-assets

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}

d47e08532d49a44b1f2705d74592f52fecce6ecd
Wika <97953@student.ue.poznan.pl>
2026-01-28 22:43:30 +0100

    Dodanie AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class AudioManager : MonoBehaviour
+    {
+        public AudioSource sfxSource;
+        public AudioClip flipSound;
+        public AudioClip matchSound;
+        public AudioClip clickSound;
+
+        public void PlayFlip() => sfxSource.PlayOneShot(flipSound);
+        public void PlayMatch() => sfxSource.PlayOneShot(matchSound);
+        public void PlayClick() => sfxSource.PlayOneShot(clickSound);
+    }
+

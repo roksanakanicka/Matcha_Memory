9d2c05bb833adc7274c3a5d01582e36c2374b3fe
Wika <97953@student.ue.poznan.pl>
2026-02-01 00:01:34 +0100

    Dodanie MatchEffectPlayer i VFX

---
+++ Assets/Scripts/MatchEffectPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchEffectPlayer : MonoBehaviour
+{
+    public ParticleSystem leafSystem;
+    public ParticleSystem steamSystem;
+
+    public void PlayEffect(Vector3 position)
+    {
+        transform.position = position;
+        leafSystem.Play();
+        steamSystem.Play();
+    }
+}
